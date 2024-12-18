1ec2a2ceeb9095b9fdc9512a32f57285179170b2
Pranav9948 <pranavat9948@gmail.com>
2024-12-18 15:08:08 +0530

    added button as response

--- server.js
+++ server.js
@@ -25,12 +25,9 @@ app.get("/webhook", (req, res) => {
   let challenge = req.query["hub.challenge"];
   let token = req.query["hub.verify_token"];
 
-
-
   if (mode && token) {
-
-    console.log('mode'.america,mode,token,myToken)
-    if (mode === "subscribe" && myToken==token) {
+    console.log("mode".america, mode, token, myToken);
+    if (mode === "subscribe" && myToken === token) {
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
     } else {
@@ -39,73 +36,81 @@ app.get("/webhook", (req, res) => {
   }
 });
 
+app.post("/webhook", async (req, res) => {
+  console.log("req.body", req.body);
 
-app.post("/webhook", async(req, res) => {
-  console.log("req.body", req.body);  
+  let body_param = req.body;
 
-  let body_param=req.body
+  console.log(JSON.stringify(body_param, null, 2));
 
-  console.log(JSON.stringify(body_param,null,2))
-
-  if(body_param.object){
-
-    if(body_param.entry && 
+  if (body_param.object) {
+    if (
+      body_param.entry &&
       body_param.entry[0] &&
-       body_param.entry[0].changes && 
-       body_param.entry[0].changes[0] && 
-       body_param.entry[0].changes[0].value.messages && 
-       body_param.entry[0].changes[0].value.messages[0]){
+      body_param.entry[0].changes &&
+      body_param.entry[0].changes[0] &&
+      body_param.entry[0].changes[0].value.messages &&
+      body_param.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id =
+        body_param.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = body_param.entry[0].changes[0].value.messages[0].from;
+      let id = body_param.entry[0].changes[0].value.messages[0].id;
+      let timestamp =
+        body_param.entry[0].changes[0].value.messages[0].timestamp;
+      let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
 
-        let phone_number_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-        let from = body_param.entry[0].changes[0].value.messages[0].from;
-        let id = body_param.entry[0].changes[0].value.messages[0].id;
-        let timestamp = body_param.entry[0].changes[0].value.messages[0].timestamp;
-        let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-
-
-        try {
-          const config = {
-            method: "post",
-            url: `https://graph.facebook.com/${process.env.VERSION}/${process.env.PHONE_NUMBER_ID}/messages`,
-            headers: {
-              Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-              "Content-Type": "application/json",
-            },
-            data:{
-
-              messaging_product: "whatsapp",
-              recipient_type: "individual",
-              to: from,
-              type: "text",
-              text: {
-                body: "Thank you for contacting us. We will get back to you shortly.",
+      try {
+        const config = {
+          method: "post",
+          url: `https://graph.facebook.com/${process.env.VERSION}/${process.env.PHONE_NUMBER_ID}/messages`,
+          headers: {
+            Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+            "Content-Type": "application/json",
+          },
+          data: {
+            messaging_product: "whatsapp",
+            recipient_type: "individual",
+            to: from,
+            type: "interactive",
+            interactive: {
+              type: "button",
+              body: {
+                text: "Please choose one of the options below:",
               },
-
-
+              action: {
+                buttons: [
+                  {
+                    type: "reply",
+                    reply: {
+                      id: "option_1",
+                      title: "Option 1",
+                    },
+                  },
+                  {
+                    type: "reply",
+                    reply: {
+                      id: "option_2",
+                      title: "Option 2",
+                    },
+                  },
+                ],
+              },
             },
-          };
-      
-          const response = await axios(config);
-          console.log("Message sent successfully:", response.data);
-          return res.status(200).send("Message sent successfully");
-        } catch (error) {
-      
-          
-          console.log("err".america, error);
+          },
+        };
 
+        const response = await axios(config);
+        console.log("Message sent successfully:", response.data);
+        return res.status(200).send("Message sent successfully");
+      } catch (error) {
+        console.log("err".america, error);
+      }
+    } else {
+      res.status(404).send("Invalid request");
     }
-
   }
-
-  else {
-
-    res.status(404).send("Invalid request");
-  }
-
-}
-
-
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
